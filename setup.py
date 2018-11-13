d71c32201868045209a26ee2d5d35c9b60a0d982
TriKriSta <Trikrista13@gmail.com>
2018-11-13 14:58:49 +0200

    rename electrum_dash on electrum_sibcoin in source files

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ with open('contrib/requirements/requirements.txt') as f:
 with open('contrib/requirements/requirements-hw.txt') as f:
     requirements_hw = f.read().splitlines()
 
-version = imp.load_source('version', 'electrum_dash/version.py')
+version = imp.load_source('version', 'electrum_sibcoin/version.py')
 
 if sys.version_info[:3] < (3, 4, 0):
-    sys.exit("Error: Dash-Electrum requires Python version >= 3.4.0...")
+    sys.exit("Error: Sibcoin-Electrum requires Python version >= 3.4.0...")
 
 data_files = []
 
@@ -39,8 +39,8 @@ if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
         else:
             usr_share = os.path.expanduser('~/.local/share')
     data_files += [
-        (os.path.join(usr_share, 'applications/'), ['electrum-dash.desktop']),
-        (os.path.join(usr_share, icons_dirname), ['icons/electrum-dash.png'])
+        (os.path.join(usr_share, 'applications/'), ['electrum-sibcoin.desktop']),
+        (os.path.join(usr_share, icons_dirname), ['icons/electrum-sibcoin.png'])
     ]
 
 extras_require = {
@@ -61,7 +61,7 @@ class CustomInstallCommand(install):
             pass
         else:
             try:
-                path = os.path.join(self.install_lib, "electrum_dash/gui/qt/icons_rc.py")
+                path = os.path.join(self.install_lib, "electrum_sibcoin/gui/qt/icons_rc.py")
                 if not os.path.exists(path):
                     subprocess.call(["pyrcc5", "icons.qrc", "-o", path])
             except Exception as e:
@@ -69,33 +69,33 @@ class CustomInstallCommand(install):
 
 
 setup(
-    name="Dash-Electrum",
+    name="Sibcoin-Electrum",
     version=version.ELECTRUM_VERSION,
     install_requires=requirements,
     extras_require=extras_require,
     packages=[
-        'electrum_dash',
-        'electrum_dash.gui',
-        'electrum_dash.gui.qt',
-    ] + [('electrum_dash.plugins.'+pkg) for pkg in find_packages('electrum_dash/plugins')],
+        'electrum_sibcoin',
+        'electrum_sibcoin.gui',
+        'electrum_sibcoin.gui.qt',
+    ] + [('electrum_sibcoin.plugins.'+pkg) for pkg in find_packages('electrum_sibcoin/plugins')],
     package_dir={
-        'electrum_dash': 'electrum_dash'
+        'electrum_sibcoin': 'electrum_sibcoin'
     },
     package_data={
         '': ['*.txt', '*.json', '*.ttf', '*.otf'],
-        'electrum_dash': [
+        'electrum_sibcoin': [
             'wordlist/*.txt',
             'locale/*/LC_MESSAGES/electrum.mo',
         ],
     },
-    scripts=['electrum_dash/electrum-dash'],
+    scripts=['electrum_sibcoin/electrum-sibcoin'],
     data_files=data_files,
-    description="Lightweight Dashpay Wallet",
+    description="Lightweight Sibcoinpay Wallet",
     maintainer="akhavr",
     maintainer_email="akhavr@khavr.com",
     license="MIT License",
-    url="https://electrum.dash.org",
-    long_description="""Lightweight Dashpay Wallet""",
+    url="https://sibcoin.money",
+    long_description="""Lightweight Sibcoin Wallet""",
     cmdclass={
         'install': CustomInstallCommand,
     },
