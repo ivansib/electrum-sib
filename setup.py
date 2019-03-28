d354777a6f6ab1a2db8126bdfc678fdf2fe9fd7d
TriKriSta <Trikrista13@gmail.com>
2019-03-28 12:57:09 +0200

    edit setup

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ setup(
         'electrum_sibcoin',
         'electrum_sibcoin.gui',
         'electrum_sibcoin.gui.qt',
+        'electrum_sibcoin.plugins',
     ] + [('electrum_sibcoin.plugins.'+pkg) for pkg in find_packages('electrum_sibcoin/plugins')],
     package_dir={
         'electrum_sibcoin': 'electrum_sibcoin'
