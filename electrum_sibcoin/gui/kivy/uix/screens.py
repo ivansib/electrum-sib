AXErunners/electrum-axe

changes from upstream up to 3.3.7

electrum_dash/gui/kivy/uix/screens.py
@@ -20,11 +20,12 @@ from kivy.utils import platform
 
 from electrum_dash.util import profiler, parse_URI, format_time, InvalidPassword, NotEnoughFunds, Fiat
 from electrum_dash import bitcoin
-from electrum_dash .transaction import TxOutput
-from electrum_dash.util import send_exception_to_crash_reporter
+from electrum_dash.transaction import TxOutput, Transaction, tx_from_str
+from electrum_dash.util import send_exception_to_crash_reporter, parse_URI, InvalidBitcoinURI
 from electrum_dash.paymentrequest import PR_UNPAID, PR_PAID, PR_UNKNOWN, PR_EXPIRED
 from electrum_dash.plugin import run_hook
 from electrum_dash.wallet import InternalAddressCorruption
+from electrum_dash import simple_config
 
 from .context_menu import ContextMenu
 
@@ -173,11 +174,10 @@ class SendScreen(CScreen):
         if not self.app.wallet:
             self.payment_request_queued = text
             return
-        import electrum_dash
         try:
-            uri = electrum_dash.util.parse_URI(text, self.app.on_pr)
-        except:
-            self.app.show_info(_("Not a Dash URI"))
+            uri = parse_URI(text, self.app.on_pr, loop=self.app.asyncio_loop)
+        except InvalidBitcoinURI as e:
+            self.app.show_info(_("Error parsing URI") + f":\n{e}")
             return
         amount = uri.get('amount')
         self.screen.address = uri.get('address', '')
@@ -233,11 +233,22 @@ class SendScreen(CScreen):
             self.payment_request = None
 
     def do_paste(self):
-        contents = self.app._clipboard.paste()
-        if not contents:
+        data = self.app._clipboard.paste()
+        if not data:
             self.app.show_info(_("Clipboard is empty"))
             return
-        self.set_URI(contents)
+        # try to decode as transaction
+        try:
+            raw_tx = tx_from_str(data)
+            tx = Transaction(raw_tx)
+            tx.deserialize()
+        except:
+            tx = None
+        if tx:
+            self.app.tx_dialog(tx)
+            return
+        # try to decode as URI/address
+        self.set_URI(data)
 
     def do_send(self):
         if self.screen.is_pr:
@@ -286,8 +297,9 @@ class SendScreen(CScreen):
             x_fee_address, x_fee_amount = x_fee
             msg.append(_("Additional fees") + ": " + self.app.format_amount_and_units(x_fee_amount))
 
-        if fee >= config.get('confirm_fee', 10000):
-            msg.append(_('Warning')+ ': ' + _("The fee for this transaction seems unusually high."))
+        feerate_warning = simple_config.FEERATE_WARNING_HIGH_FEE
+        if fee > feerate_warning * tx.estimated_size() / 1000:
+            msg.append(_('Warning') + ': ' + _("The fee for this transaction seems unusually high."))
         msg.append(_("Enter your PIN code to proceed"))
         self.app.protected('\n'.join(msg), self.send_tx, (tx, message))
 

electrum_dash/gui/qt/address_list.py
@@ -31,7 +31,7 @@ from PyQt5.QtGui import QStandardItemModel, QStandardItem, QFont
 from PyQt5.QtWidgets import QAbstractItemView, QComboBox, QLabel, QMenu
 
 from electrum_dash.i18n import _
-from electrum_dash.util import block_explorer_URL
+from electrum_dash.util import block_explorer_URL, profiler
 from electrum_dash.plugin import run_hook
 from electrum_dash.bitcoin import is_address
 from electrum_dash.wallet import InternalAddressCorruption
@@ -107,6 +107,7 @@ class AddressList(MyTreeView):
         self.show_used = state
         self.update()
 
+    @profiler
     def update(self):
         self.wallet = self.parent.wallet
         current_address = self.current_item_user_role(col=self.Columns.LABEL)
@@ -187,6 +188,8 @@ class AddressList(MyTreeView):
         menu = QMenu()
         if not multi_select:
             idx = self.indexAt(position)
+            if not idx.isValid():
+                return
             col = idx.column()
             item = self.model().itemFromIndex(idx)
             if not item:

ivansib/electrum-sib

electrum_sibcoin/gui/kivy/uix/screens.py
@@ -20,11 +20,12 @@ from kivy.utils import platform
 
 from electrum_sibcoin.util import profiler, parse_URI, format_time, InvalidPassword, NotEnoughFunds, Fiat
 from electrum_sibcoin import bitcoin
-from electrum_sibcoin .transaction import TxOutput
-from electrum_sibcoin.util import send_exception_to_crash_reporter
+from electrum_sibcoin.transaction import TxOutput, Transaction, tx_from_str
+from electrum_sibcoin.util import send_exception_to_crash_reporter, parse_URI, InvalidBitcoinURI
 from electrum_sibcoin.paymentrequest import PR_UNPAID, PR_PAID, PR_UNKNOWN, PR_EXPIRED
 from electrum_sibcoin.plugin import run_hook
-from electrum_dash.wallet import InternalAddressCorruption
+from electrum_sibcoin.wallet import InternalAddressCorruption
+from electrum_sibcoin import simple_config
 
 from .context_menu import ContextMenu
 
@@ -173,11 +174,10 @@ class SendScreen(CScreen):
         if not self.app.wallet:
             self.payment_request_queued = text
             return
-        import electrum_sibcoin
         try:
-            uri = electrum_sibcoin.util.parse_URI(text, self.app.on_pr)
-        except:
-            self.app.show_info(_("Not a Sibcoin URI"))
+            uri = parse_URI(text, self.app.on_pr, loop=self.app.asyncio_loop)
+        except InvalidBitcoinURI as e:
+            self.app.show_info(_("Error parsing URI") + f":\n{e}")
             return
         amount = uri.get('amount')
         self.screen.address = uri.get('address', '')
@@ -233,11 +233,22 @@ class SendScreen(CScreen):
             self.payment_request = None
 
     def do_paste(self):
-        contents = self.app._clipboard.paste()
-        if not contents:
+        data = self.app._clipboard.paste()
+        if not data:
             self.app.show_info(_("Clipboard is empty"))
             return
-        self.set_URI(contents)
+        # try to decode as transaction
+        try:
+            raw_tx = tx_from_str(data)
+            tx = Transaction(raw_tx)
+            tx.deserialize()
+        except:
+            tx = None
+        if tx:
+            self.app.tx_dialog(tx)
+            return
+        # try to decode as URI/address
+        self.set_URI(data)
 
     def do_send(self):
         if self.screen.is_pr:
@@ -286,8 +297,9 @@ class SendScreen(CScreen):
             x_fee_address, x_fee_amount = x_fee
             msg.append(_("Additional fees") + ": " + self.app.format_amount_and_units(x_fee_amount))
 
-        if fee >= config.get('confirm_fee', 10000):
-            msg.append(_('Warning')+ ': ' + _("The fee for this transaction seems unusually high."))
+        feerate_warning = simple_config.FEERATE_WARNING_HIGH_FEE
+        if fee > feerate_warning * tx.estimated_size() / 1000:
+            msg.append(_('Warning') + ': ' + _("The fee for this transaction seems unusually high."))
         msg.append(_("Enter your PIN code to proceed"))
         self.app.protected('\n'.join(msg), self.send_tx, (tx, message))
 

electrum_sibcoin/gui/qt/address_list.py
@@ -31,7 +31,7 @@ from PyQt5.QtGui import QStandardItemModel, QStandardItem, QFont
 from PyQt5.QtWidgets import QAbstractItemView, QComboBox, QLabel, QMenu
 
 from electrum_sibcoin.i18n import _
-from electrum_sibcoin.util import block_explorer_URL
+from electrum_sibcoin.util import block_explorer_URL, profiler
 from electrum_sibcoin.plugin import run_hook
 from electrum_sibcoin.bitcoin import is_address
 from electrum_sibcoin.wallet import InternalAddressCorruption
@@ -107,6 +107,7 @@ class AddressList(MyTreeView):
         self.show_used = state
         self.update()
 
+    @profiler
     def update(self):
         self.wallet = self.parent.wallet
         current_address = self.current_item_user_role(col=self.Columns.LABEL)
@@ -176,7 +177,7 @@ class AddressList(MyTreeView):
         self.filter()
 
     def create_menu(self, position):
-        from electrum_sibcoin.wallet import Multisig_Wallet
+        from electrum_dash.wallet import Multisig_Wallet
         is_multisig = isinstance(self.wallet, Multisig_Wallet)
         can_delete = self.wallet.can_delete_address()
         selected = self.selected_in_column(self.Columns.ADDRESS)
@@ -187,6 +188,8 @@ class AddressList(MyTreeView):
         menu = QMenu()
         if not multi_select:
             idx = self.indexAt(position)
+            if not idx.isValid():
+                return
             col = idx.column()
             item = self.model().itemFromIndex(idx)
             if not item: