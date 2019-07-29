AXErunners/electrum-axe

fix testnet name for safet, keepkey plugins

electrum_dash/plugins/keepkey/keepkey.py
@@ -175,7 +175,7 @@ class KeepKeyPlugin(HW_PluginBase):
         return client
 
     def get_coin_name(self):
-        return "DashTestnet" if constants.net.TESTNET else "Dash"
+        return "Dash Testnet" if constants.net.TESTNET else "Dash"
 
     def initialize_device(self, device_id, wizard, handler):
         # Initialization method

electrum_dash/plugins/safe_t/safe_t.py
@@ -159,7 +159,7 @@ class SafeTPlugin(HW_PluginBase):
         return client
 
     def get_coin_name(self):
-        return "DashTestnet" if constants.net.TESTNET else "Dash"
+        return "Dash Testnet" if constants.net.TESTNET else "Dash"
 
     def initialize_device(self, device_id, wizard, handler):
         # Initialization method

ivansib/electrum-sib

electrum_sibcoin/plugins/keepkey/keepkey.py
@@ -175,7 +175,7 @@ class KeepKeyPlugin(HW_PluginBase):
         return client
 
     def get_coin_name(self):
-        return "SibcoinTestnet" if constants.net.TESTNET else "Sibcoin"
+        return "Sibcoin Testnet" if constants.net.TESTNET else "Sibcoin"
 
     def initialize_device(self, device_id, wizard, handler):
         # Initialization method

electrum_sibcoin/plugins/safe_t/safe_t.py
@@ -159,7 +159,7 @@ class SafeTPlugin(HW_PluginBase):
         return client
 
     def get_coin_name(self):
-        return "SibcoinTestnet" if constants.net.TESTNET else "Sibcoin"
+        return "Sibcoin Testnet" if constants.net.TESTNET else "Sibcoin"
 
     def initialize_device(self, device_id, wizard, handler):
         # Initialization method
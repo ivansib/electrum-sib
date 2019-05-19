8683ae9c40be5b66c06e4e8b60efbeaf0d999bd9
Builder <builder@example.com>
2019-05-20 00:48:48 +0300

    fix testnet name for safet, keepkey plugins

--- electrum_sibcoin/plugins/keepkey/keepkey.py
+++ electrum_sibcoin/plugins/keepkey/keepkey.py
@@ -175,7 +175,7 @@ class KeepKeyPlugin(HW_PluginBase):
         return client
 
     def get_coin_name(self):
-        return "SibcoinTestnet" if constants.net.TESTNET else "Sibcoin"
+        return "Sibcoin Testnet" if constants.net.TESTNET else "Sibcoin"
 
     def initialize_device(self, device_id, wizard, handler):
         # Initialization method
--- electrum_sibcoin/plugins/safe_t/safe_t.py
+++ electrum_sibcoin/plugins/safe_t/safe_t.py
@@ -159,7 +159,7 @@ class SafeTPlugin(HW_PluginBase):
         return client
 
     def get_coin_name(self):
-        return "SibcoinTestnet" if constants.net.TESTNET else "Sibcoin"
+        return "Sibcoin Testnet" if constants.net.TESTNET else "Sibcoin"
 
     def initialize_device(self, device_id, wizard, handler):
         # Initialization method
