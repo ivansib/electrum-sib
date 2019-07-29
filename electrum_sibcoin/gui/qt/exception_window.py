AXErunners/electrum-axe

changes from upstream up to 3.3.8

electrum_dash/gui/qt/exception_window.py
@@ -33,6 +33,8 @@ from PyQt5.QtWidgets import (QWidget, QLabel, QPushButton, QTextEdit,
 from electrum_dash.i18n import _
 from electrum_dash.base_crash_reporter import BaseCrashReporter
 from electrum_dash.logging import Logger
+from electrum_dash import constants
+
 from .util import MessageBoxMixin, read_QIcon, WaitingDialog
 
 
@@ -97,17 +99,22 @@ class Exception_Window(BaseCrashReporter, QWidget, MessageBoxMixin, Logger):
 
     def send_report(self):
         def on_success(response):
+            # note: 'response' coming from (remote) crash reporter server.
+            # It contains a URL to the GitHub issue, so we allow rich text.
             self.show_message(parent=self,
                               title=_("Crash report"),
-                              msg=response)
+                              msg=response,
+                              rich_text=True)
             self.close()
         def on_failure(exc_info):
             e = exc_info[1]
             self.logger.error('There was a problem with the automatic reporting', exc_info=exc_info)
             self.show_critical(parent=self,
-                               msg=(_('There was a problem with the automatic reporting:') + '\n' +
-                                    str(e) + '\n' +
-                                    _("Please report this issue manually.")))
+                               msg=(_('There was a problem with the automatic reporting:') + '<br/>' +
+                                    repr(e)[:120] + '<br/>' +
+                                    _("Please report this issue manually") +
+                                    f' <a href="{constants.GIT_REPO_ISSUES_URL}">on GitHub</a>.'),
+                               rich_text=True)
 
         proxy = self.main_window.network.proxy
         task = lambda: BaseCrashReporter.send_report(self, self.main_window.network.asyncio_loop, proxy)

electrum_dash/gui/qt/main_window.py
@@ -30,7 +30,6 @@ import traceback
 import json
 import shutil
 import weakref
-import webbrowser
 import csv
 from decimal import Decimal
 import base64
@@ -88,7 +87,7 @@ from .util import (read_QIcon, ColorScheme, text_dialog, icon_path, WaitingDialo
                    OkButton, InfoButton, WWLabel, TaskThread, CancelButton,
                    CloseButton, HelpButton, MessageBoxMixin, EnterButton, expiration_values,
                    ButtonsLineEdit, CopyCloseButton, import_meta_gui, export_meta_gui,
-                   filename_field, address_field, char_width_in_lineedit)
+                   filename_field, address_field, char_width_in_lineedit, webopen)
 from .installwizard import WIF_HELP_TEXT
 from .history_list import HistoryList, HistoryModel
 from .update_checker import UpdateCheck, UpdateCheckThread
@@ -676,9 +675,9 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
         help_menu = menubar.addMenu(_("&Help"))
         help_menu.addAction(_("&About"), self.show_about)
         help_menu.addAction(_("&Check for updates"), self.show_update_check)
-        help_menu.addAction(_("&Official website"), lambda: webbrowser.open("https://electrum.dash.org"))
+        help_menu.addAction(_("&Official website"), lambda: webopen("https://electrum.dash.org"))
         help_menu.addSeparator()
-        help_menu.addAction(_("&Documentation"), lambda: webbrowser.open("https://docs.dash.org/en/latest/wallets/index.html#dash-electrum-wallet")).setShortcut(QKeySequence.HelpContents)
+        help_menu.addAction(_("&Documentation"), lambda: webopen("https://docs.dash.org/en/latest/wallets/index.html#dash-electrum-wallet")).setShortcut(QKeySequence.HelpContents)
         self._auto_crash_reports = QAction(_("&Automated Crash Reports"), self, checkable=True)
         self._auto_crash_reports.setChecked(self.config.get(BaseCrashReporter.config_key, default=False))
         self._auto_crash_reports.triggered.connect(self.auto_crash_reports)
@@ -717,7 +716,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
     def show_report_bug(self):
         msg = ' '.join([
             _("Please report any bugs as issues on github:<br/>"),
-            "<a href=\"https://github.com/akhavr/electrum-dash/issues\">https://github.com/akhavr/electrum-dash/issues</a><br/><br/>",
+            f'''<a href="{constants.GIT_REPO_ISSUES_URL}">{constants.GIT_REPO_ISSUES_URL}</a><br/><br/>''',
             _("Before reporting a bug, upgrade to the most recent version of Dash Electrum (latest release or git HEAD), and include the version number in your report."),
             _("Try to explain not only what the bug is, but how it occurs.")
          ])

ivansib/electrum-sib

electrum_sibcoin/gui/qt/exception_window.py
@@ -33,6 +33,7 @@ from PyQt5.QtWidgets import (QWidget, QLabel, QPushButton, QTextEdit,
 from electrum_sibcoin.i18n import _
 from electrum_sibcoin.base_crash_reporter import BaseCrashReporter
 from electrum_sibcoin.logging import Logger
+from electrum_sibcoin import constants
 from .util import MessageBoxMixin, read_QIcon, WaitingDialog
 
 
@@ -97,17 +98,22 @@ class Exception_Window(BaseCrashReporter, QWidget, MessageBoxMixin, Logger):
 
     def send_report(self):
         def on_success(response):
+            # note: 'response' coming from (remote) crash reporter server.
+            # It contains a URL to the GitHub issue, so we allow rich text.
             self.show_message(parent=self,
                               title=_("Crash report"),
-                              msg=response)
+                              msg=response,
+                              rich_text=True)
             self.close()
         def on_failure(exc_info):
             e = exc_info[1]
             self.logger.error('There was a problem with the automatic reporting', exc_info=exc_info)
             self.show_critical(parent=self,
-                               msg=(_('There was a problem with the automatic reporting:') + '\n' +
-                                    str(e) + '\n' +
-                                    _("Please report this issue manually.")))
+                               msg=(_('There was a problem with the automatic reporting:') + '<br/>' +
+                                    repr(e)[:120] + '<br/>' +
+                                    _("Please report this issue manually") +
+                                    f' <a href="{constants.GIT_REPO_ISSUES_URL}">on GitHub</a>.'),
+                               rich_text=True)
 
         proxy = self.main_window.network.proxy
         task = lambda: BaseCrashReporter.send_report(self, self.main_window.network.asyncio_loop, proxy)

electrum_sibcoin/gui/qt/main_window.py
@@ -88,7 +88,7 @@ from .util import (read_QIcon, ColorScheme, text_dialog, icon_path, WaitingDialo
                    OkButton, InfoButton, WWLabel, TaskThread, CancelButton,
                    CloseButton, HelpButton, MessageBoxMixin, EnterButton, expiration_values,
                    ButtonsLineEdit, CopyCloseButton, import_meta_gui, export_meta_gui,
-                   filename_field, address_field, char_width_in_lineedit)
+                   filename_field, address_field, char_width_in_lineedit, webopen)
 from .installwizard import WIF_HELP_TEXT
 from .history_list import HistoryList, HistoryModel
 from .update_checker import UpdateCheck, UpdateCheckThread
@@ -676,9 +676,9 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
         help_menu = menubar.addMenu(_("&Help"))
         help_menu.addAction(_("&About"), self.show_about)
         help_menu.addAction(_("&Check for updates"), self.show_update_check)
-        help_menu.addAction(_("&Official website"), lambda: webbrowser.open("https://electrum.sibcoin.org"))
+        help_menu.addAction(_("&Official website"), lambda: webopen("https://electrum.sibcoin.org"))
         help_menu.addSeparator()
-        help_menu.addAction(_("&Documentation"), lambda: webbrowser.open("https://docs.sibcoin.org/en/latest/wallets/index.html#dash-electrum-wallet")).setShortcut(QKeySequence.HelpContents)
+        help_menu.addAction(_("&Documentation"), lambda: webopen("https://docs.sibcoin.org/en/latest/wallets/index.html#dash-electrum-wallet")).setShortcut(QKeySequence.HelpContents)
         self._auto_crash_reports = QAction(_("&Automated Crash Reports"), self, checkable=True)
         self._auto_crash_reports.setChecked(self.config.get(BaseCrashReporter.config_key, default=False))
         self._auto_crash_reports.triggered.connect(self.auto_crash_reports)
@@ -717,7 +717,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger):
     def show_report_bug(self):
         msg = ' '.join([
             _("Please report any bugs as issues on github:<br/>"),
-            "<a href=\"https://github.com/akhavr/electrum-dash/issues\">https://github.com/akhavr/electrum-dash/issues</a><br/><br/>",
+            f'''<a href="{constants.GIT_REPO_ISSUES_URL}">{constants.GIT_REPO_ISSUES_URL}</a><br/><br/>''',
             _("Before reporting a bug, upgrade to the most recent version of Sibcoin-Electrum (latest release or git HEAD), and include the version number in your report."),
             _("Try to explain not only what the bug is, but how it occurs.")
          ])